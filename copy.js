c8d15ee05fb08d2a348ef30e16a95e5747d8a26a
Gagik <gagikpog@gmail.com>
2023-05-13 23:58:06 +0500

    add copy button

---
+++ copy.js
@@ -0,0 +1,41 @@
+function copyLink() {
+    const hostname = `${window.location.protocol}//${window.location.hostname}`;
+    const checked = $("#checkbox1").val() === 'on';
+    const list = players.map((item) => item.text).join('$_$');
+    copyTextToClipboard(`${hostname}/?checked=${checked}&list=${list}`);
+}
+
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        return fallbackCopyTextToClipboard(text);
+    }
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
+
+function fallbackCopyTextToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        const successful = document.execCommand('copy');
+        const msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
