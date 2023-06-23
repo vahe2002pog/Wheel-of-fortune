0418ca7474e5cadce5b4c5ff62218337f28f9c3a
Gagik <gagikpog@gmail.com>
2023-06-23 14:13:22 +0500

    create en page with gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var replace = require('gulp-string-replace');
+
+gulp.task('default', function() {
+    return gulp.src('./build/index.html')
+        .pipe(replace('lang="ru"', 'lang="en"'))
+        .pipe(replace('Колесо фортуны «Колесо удачи»', 'Wheel of Fortune'))
+        .pipe(replace('Колесо удачи или фортуны онлайн. Простой способ не делать выбор самому и надеется на случай. Выбор из нескольких вариантов или выбор с помощью исключения вариантов', 'Wheel of fortune or fortune online. An easy way to not make the choice yourself and hope for a chance. Select from multiple options or select by dropping options'))
+        .pipe(replace('Фортуна', 'Fortune'))
+        .pipe(replace('На выбивание', 'For knockout'))
+        .pipe(replace('Игроки', 'Players'))
+        .pipe(gulp.dest('./build/en/'));
+});
