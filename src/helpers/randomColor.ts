e6c7c62b5698c62e68deb44d767bc70ea93f6a3a
Gagik <gagikpog@gmail.com>
2023-07-09 23:16:22 +0500

    new random color

--- src/helpers/randomColor.ts
+++ src/helpers/randomColor.ts
@@ -1,8 +1,21 @@
+function * randomGenerator(previous: number = 0, max: number) {
+    while (true) {
+        previous = previous * 16807 % 2147483647
+        yield previous % max;
+    }
+}
+
+const r = randomGenerator(1, 16);
+
+function random(): number {
+    return r.next().value as number;
+}
+
 export function getRandomColor(): string {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(random())];
     }
     return color;
 }
