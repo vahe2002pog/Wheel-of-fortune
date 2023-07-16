e98d9d4dc9fc488914c285ffe152b32ae921b260
Gagik <gagikpog@gmail.com>
2023-07-16 16:52:22 +0500

    new logo

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('default', function() {
         .pipe(replace('lang="ru"', 'lang="en"'))
         .pipe(replace('Колесо фортуны «Колесо удачи»', 'Wheel of Fortune'))
         .pipe(replace('Колесо удачи или фортуны онлайн. Простой способ не делать выбор самому и надеется на случай. Выбор из нескольких вариантов или выбор с помощью исключения вариантов', 'Wheel of fortune online. An easy way to not make the choice yourself and hope for a chance. Select from multiple options or select by dropping options'))
-        .pipe(replace('Фортуна', 'Fortune'))
+        .pipe(replace('Фортуна онлайн', 'Fortune online'))
         .pipe(replace('На выбывание', 'For knockout'))
         .pipe(replace('Игроки', 'Players'))
         .pipe(gulp.dest('./build/en/'));
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import copyIcon from '../img/copy.svg';
 import pasteIcon from '../img/paste.svg';
+import logo from '../img/logo.svg';
 import { useTranslation } from 'react-i18next';
 import Noindex from './noindex';
 
@@ -14,7 +15,7 @@ export default function Header(props: IProps) {
     const { t } = useTranslation();
     return (
         <header className="tw-flex">
-            <span>{t('header.title')}</span>
+            <img src={logo} alt={ t('header.title') } height='30px' />
             <Noindex>
                 <div className="links-wrapper">
                     <img src={pasteIcon} onClick={() => props.onPaste()} className="svg-icon tw-cursor-pointer" alt="paste icon" width="24px" height="24px" title={t('header.paste')} />
