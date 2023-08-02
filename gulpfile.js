f4ba51963caf3b15f3be171decaaae92f8422532
Gagik <gagikpog@gmail.com>
2023-08-02 21:29:44 +0500

    update en description

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task('default', function() {
     return gulp.src('./build/index.html')
         .pipe(replace('lang="ru"', 'lang="en"'))
         .pipe(replace('Колесо фортуны «Колесо удачи или рандома»', 'Wheel of Fortune'))
-        .pipe(replace('Колесо удачи или фортуны онлайн. Простой способ не делать выбор самому и надеется на случай. Выбор из нескольких вариантов или выбор с помощью исключения вариантов', 'Wheel of fortune online. An easy way to not make the choice yourself and hope for a chance. Select from multiple options or select by dropping options'))
+        .pipe(replace('Колесо удачи или фортуны онлайн. Простой способ не делать выбор самому и надеется на случай. Выбор из нескольких вариантов или выбор с помощью исключения вариантов', 'Experience the thrill of chance with the online Wheel of Fortune. Take the easy route and let fate decide for you. Choose from multiple options or eliminate them one by one to reach your desired outcome.'))
         .pipe(replace('Колесо Фортуны онлайн', 'Wheel of Fortune online'))
         .pipe(replace('На выбывание', 'Elimination mode'))
         .pipe(replace('Игроки', 'Players'))
